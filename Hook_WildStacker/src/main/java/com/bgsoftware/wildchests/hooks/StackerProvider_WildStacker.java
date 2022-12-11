660ecfd839392a84530cdd700a4c773a99f775bb
dan28000 <84990628+dan28000@users.noreply.github.com>
2022-12-11 22:00:57 +0100

    update

--- Hook_WildStacker/src/main/java/com/bgsoftware/wildchests/hooks/StackerProvider_WildStacker.java
+++ Hook_WildStacker/src/main/java/com/bgsoftware/wildchests/hooks/StackerProvider_WildStacker.java
@@ -3,14 +3,12 @@ package com.bgsoftware.wildchests.hooks;
 import com.bgsoftware.wildchests.api.hooks.StackerProvider;
 import com.bgsoftware.wildstacker.WildStackerPlugin;
 import com.bgsoftware.wildstacker.api.WildStackerAPI;
-import com.bgsoftware.wildstacker.api.enums.SpawnCause;
 import com.bgsoftware.wildstacker.api.objects.StackedItem;
 import org.bukkit.Location;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 
 public final class StackerProvider_WildStacker implements StackerProvider {
-
     @Override
     public int getItemAmount(Item item) {
         return WildStackerAPI.getItemAmount(item);
@@ -23,12 +21,10 @@ public final class StackerProvider_WildStacker implements StackerProvider {
 
     @Override
     public boolean dropItem(Location location, ItemStack itemStack, int amount) {
-        StackedItem item = WildStackerPlugin.getPlugin().getNMSAdapter().createItem(location, itemStack, SpawnCause.CUSTOM,
-                (stackedItem) -> {
-                    stackedItem.setStackAmount(1, stackedItem.isCached());
-                });
+        StackedItem item = WildStackerPlugin.getPlugin().getSystemManager().spawnItemWithAmount(location, itemStack, 1);
         item.getItem().setPickupDelay(20);
         item.setStackAmount(amount, true);
+        item.updateName();
         return true;
     }
 
